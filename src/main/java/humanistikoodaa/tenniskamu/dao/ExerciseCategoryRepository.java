5ab115e878c1902deed7f6844f915b0c1f2d5f15
Henrik Lyra <henrik.lyra@stat.fi>
2023-06-02 16:40:08 +0300

    Rename excercise -> exercise

--- src/main/java/humanistikoodaa/tenniskamu/dao/ExerciseCategoryRepository.java
+++ src/main/java/humanistikoodaa/tenniskamu/dao/ExerciseCategoryRepository.java
@@ -3,9 +3,9 @@ package humanistikoodaa.tenniskamu.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import humanistikoodaa.tenniskamu.entity.ExcerciseCategory;
+import humanistikoodaa.tenniskamu.entity.ExerciseCategory;
 
-@RepositoryRestResource(collectionResourceRel = "excerciseCategory", path = "excercise-category")
+@RepositoryRestResource(collectionResourceRel = "exerciseCategory", path = "exercise-category")
 public interface ExcerciseCategoryRepository extends JpaRepository<ExcerciseCategory, Long>{
 
 }
